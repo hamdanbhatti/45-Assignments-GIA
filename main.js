4b729e942ee7844c68ba6fce1fc603eb928e42cc
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-03-25 03:43:57 +0500

    45-Assignments

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ export {};
 //   `“Hello ${personName} would you like to learn some Python/Typescript today?”`
 // );
 /* =======>>>>>> Assignment 3 <<<<<======== */
-/* Name Cases;( LowerCase, UpperCase, TitleCase ) */
+/* Name Cases;( LowerCase, UpperCase, TitleCase ) */ // Date 24-march-24
 // let personName: string = "hAmDan";
 // console.log(personName.toLowerCase()); //  lowerCase
 // console.log(personName.toUpperCase()); //  UPPERCASE
@@ -30,7 +30,7 @@ export {};
 // let message: string = `once said, "Our greatest weakness lies in giving up.The most certain way to succeed is always to try just one more time."`;
 // console.log(`${famous_Person} ${message}`);
 /* =======>>>>>> Assignment 6 <<<<<======== */
-/* Stripping Name */
+/* Stripping Name */ // Date 24-march-24
 // let personName: string = "\t Hamdan Bhatti \n";
 // console.log(`Name with WhiteSpaces: ${personName}`);
 // let strippedName = personName.trim();
@@ -53,3 +53,30 @@ export {};
 // console.log("-".repeat(5));
 /* =======>>>>>> Assignment 9 <<<<<======== */
 /* Favorite Number */
+// let favNum: number = 9;
+// console.log(`My Favorite number is ${favNum}`);
+/* =======>>>>>> Assignment 10 <<<<<======== */
+/*Adding Comments */
+// I created two program.One is describing the lowercase upper case and TitleCase
+// And the other program is to describing stripping names
+// Program #1 =========>>>
+/* Name Cases;( LowerCase, UpperCase, TitleCase ) */ // Date 24-march-24 M.Hamdan
+// let personName: string = "hAmDan";
+// console.log(personName.toLowerCase()); //  lowerCase
+// console.log(personName.toUpperCase()); //  UPPERCASE
+// let titleCase =
+//   personName.charAt(0).toUpperCase() + personName.toLowerCase().slice(1);
+// console.log(titleCase); // Titlecase
+// Program #2 ========>
+/* Stripping Name */ // Date 24-march-24 M.Hamdan
+// let personName: string = "\t Hamdan Bhatti \n";
+// console.log(`Name with WhiteSpaces: ${personName}`);
+// let strippedName = personName.trim();
+// console.log(`Name without WhiteSpaces:${strippedName}`);
+/* =======>>>>>> Assignment 11 <<<<<======== */
+/* Names */
+// let myFriends: string[] = ["ali", "abdul", "raheem", "omair"];
+// for (let friend of myFriends) {
+//   console.log(friend);
+// }
+/* =======>>>>>> Assignment 12 <<<<<======== */
--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@
 // );
 
 /* =======>>>>>> Assignment 3 <<<<<======== */
-/* Name Cases;( LowerCase, UpperCase, TitleCase ) */
+/* Name Cases;( LowerCase, UpperCase, TitleCase ) */ // Date 24-march-24
 
 // let personName: string = "hAmDan";
 
@@ -41,7 +41,7 @@
 // console.log(`${famous_Person} ${message}`);
 
 /* =======>>>>>> Assignment 6 <<<<<======== */
-/* Stripping Name */
+/* Stripping Name */ // Date 24-march-24
 
 // let personName: string = "\t Hamdan Bhatti \n";
 // console.log(`Name with WhiteSpaces: ${personName}`);
@@ -70,3 +70,39 @@
 
 /* =======>>>>>> Assignment 9 <<<<<======== */
 /* Favorite Number */
+
+// let favNum: number = 9;
+// console.log(`My Favorite number is ${favNum}`);
+
+/* =======>>>>>> Assignment 10 <<<<<======== */
+/*Adding Comments */
+
+// I created two program.One is describing the lowercase upper case and TitleCase
+// And the other program is to describing stripping names
+
+// Program #1 =========>>>
+
+/* Name Cases;( LowerCase, UpperCase, TitleCase ) */ // Date 24-march-24 M.Hamdan
+// let personName: string = "hAmDan";
+// console.log(personName.toLowerCase()); //  lowerCase
+// console.log(personName.toUpperCase()); //  UPPERCASE
+// let titleCase =
+//   personName.charAt(0).toUpperCase() + personName.toLowerCase().slice(1);
+// console.log(titleCase); // Titlecase
+
+// Program #2 ========>
+
+/* Stripping Name */ // Date 24-march-24 M.Hamdan
+// let personName: string = "\t Hamdan Bhatti \n";
+// console.log(`Name with WhiteSpaces: ${personName}`);
+// let strippedName = personName.trim();
+// console.log(`Name without WhiteSpaces:${strippedName}`);
+
+/* =======>>>>>> Assignment 11 <<<<<======== */
+/* Names */
+// let myFriends: string[] = ["ali", "abdul", "raheem", "omair"];
+// for (let friend of myFriends) {
+//   console.log(friend);
+// }
+
+/* =======>>>>>> Assignment 12 <<<<<======== */
