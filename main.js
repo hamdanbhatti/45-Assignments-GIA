1a04b0abeca6d5109c1052d0e0e52c77313a3fc8
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-03-24 16:47:23 +0500

    This the First upload

---
+++ main.js
@@ -0,0 +1,55 @@
+/* =======>>>>>> Assignment 1 <<<<<======== */
+export {};
+/* Install [Node.js](https://nodejs.org/en/),
+[TypeScript](https://www.typescriptlang.org/download) and
+[VS Code](https://code.visualstudio.com/) on your computer. */
+// console.log("Hello World");
+// console.log("Installation Done");
+/* =======>>>>>> Assignment 2 <<<<<======== */
+/* Personal Message */
+// let personName: string = "Hamdan";
+// console.log(
+//   `“Hello ${personName} would you like to learn some Python/Typescript today?”`
+// );
+/* =======>>>>>> Assignment 3 <<<<<======== */
+/* Name Cases;( LowerCase, UpperCase, TitleCase ) */
+// let personName: string = "hAmDan";
+// console.log(personName.toLowerCase()); //  lowerCase
+// console.log(personName.toUpperCase()); //  UPPERCASE
+// let titleCase =
+//   personName.charAt(0).toUpperCase() + personName.toLowerCase().slice(1);
+// console.log(titleCase); // Titlecase
+/* =======>>>>>> Assignment 4 <<<<<======== */
+/* Famous Quote */
+// console.log(
+//   `Thomas Edison once said, "Our greatest weakness lies in giving up.The most certain way to succeed is always to try just one more time" `
+// );
+/* =======>>>>>> Assignment 5 <<<<<======== */
+/* Famous Quote 2 */
+// let famous_Person: string = "Thomas Edison";
+// let message: string = `once said, "Our greatest weakness lies in giving up.The most certain way to succeed is always to try just one more time."`;
+// console.log(`${famous_Person} ${message}`);
+/* =======>>>>>> Assignment 6 <<<<<======== */
+/* Stripping Name */
+// let personName: string = "\t Hamdan Bhatti \n";
+// console.log(`Name with WhiteSpaces: ${personName}`);
+// let strippedName = personName.trim();
+// console.log(`Name without WhiteSpaces:${strippedName}`);
+/* =======>>>>>> Assignment 7 <<<<<======== */
+/* Number Eight*/
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(4 * 2);
+// console.log(16 / 2);
+/* =======>>>>>> Assignment 8 <<<<<======== */
+/* Creating Lines b/w console */
+// console.log(5 + 3);
+// console.log("-".repeat(5));
+// console.log(10 - 2);
+// console.log("-".repeat(5));
+// console.log(4 * 2);
+// console.log("-".repeat(5));
+// console.log(16 / 2);
+// console.log("-".repeat(5));
+/* =======>>>>>> Assignment 9 <<<<<======== */
+/* Favorite Number */
